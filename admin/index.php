d59c3ef1c96d261cccffef7b54a73fe961c9770e
DiegoRutolo <diegorutolo@gmail.com>
2019-11-25 10:58:50 +0100

    Layout de habilidades pendientes de revisión.

--- admin/index.php
+++ admin/index.php
@@ -53,15 +53,127 @@
 
           <ul class="nav nav-tabs">
             <li class="nav-item">
-              <a class="nav-link active" data-toggle="tab" href="#nueva">Nueva</a>
+              <a class="nav-link active" data-toggle="tab" href="#revisar">Revisar habilidades</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" data-toggle="tab" href="#nueva">Nueva</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" data-toggle="tab" href="#editar">Modificar</a>
             </li>
           </ul>
 
+          <!-- Contenido -->
           <div class="tab-content">
-            <div id="nueva" class="container-fluid tab-pane active form-group">
+            <!-- Revision de hechizos -->
+            <div id="revisar" class="container tab-pane active form-group">
+              <?php
+                $sql = "SELECT * FROM Habilidad WHERE Habilidad.revisar = 1";
+                $habs_pend = $con->query($sql);
+
+                if ($habs_pend->num_rows > 0) {
+                  while ($hab_pend = $habs_pend->fetch_assoc()) { ?>
+
+                    <div class="form-group">
+                      <div class="row">
+                        <div class="col-sm-8">
+                          <label >Nombre:</label>
+                          <?php echo "<input type=\"text\" class=\"form-control\" name=\"nom\" value=\"" . utf8_encode($hab_pend["nom"]) . "\" required>" ?>
+                        </div>
+                        <div class="col-sm-4">
+                          <label>Tier:</label>
+                          <?php echo "<input type=\"number\" class=\"form-control\" name=\"tier\" min=\"1\" max=\"5\" value=\"" . utf8_encode($hab_pend["tier"]) . "\" required>" ?>
+                        </div>
+                      </div>
+                      <br>
+                      <div class="row">
+                        <div class="col-sm">
+                          <label>Tipo:</label>
+                          <?php
+                            $sql = "SELECT nom FROM TipoHab WHERE TipoHab.primario = TRUE";
+                            $res = $con->query($sql);
+                          echo "<select class=\"form-control\" name=\"tipo\" value=\"" . utf8_encode($hab_pend["tipo"]) . "\" required>";
+                            if ($res->num_rows > 0) {
+                              while ($row = $res->fetch_assoc()) {
+                                echo "<option value=\"" . utf8_encode($row["nom"]) . "\">";
+                                echo utf8_encode($row["nom"]);
+                                echo "</option>\n";
+                              }
+                            }
+                            ?>
+                          </select>
+                        </div>
+                        <div class="col-sm">
+
+                          <!-- TODO: Mostrar solo los adecuados según la tabla RelTipoPrim -->
+                          <label>Subtipo (opcional):</label>
+                          <?php
+                          echo "<input type=\"text\" class=\"form-control\" name=\"subtipo\" list=\"dlSubtipos\" value=\"" . utf8_encode($hab_pend["subtipo"]) . "\">";
+
+                            $sql = "SELECT nom FROM TipoHab WHERE TipoHab.primario = FALSE";
+                            $res = $con->query($sql);
+                          ?>
+                          <datalist id="dlSubtipos">
+                            <?php
+                            if ($res->num_rows > 0) {
+                              while ($row = $res->fetch_assoc()) {
+                                echo "<option value=\"" . utf8_encode($row["nom"]) . "\">\n";
+                              }
+                            }
+                            ?>
+                          </datalist>
+                        </div>
+                        <div class="form-check">
+                          <div class="col-sm">
+                            <label class="form-check-label">
+                              <?php echo "<input type=\"checkbox\" name=\"contin\" class=\"form-check-input\" value=\"1\"" . ($hab_pend["contin"] ? "checked" : "") . ">"; ?>
+                              Continua
+                            </label>
+                          </div>
+                          <div class="col-sm">
+                            <label class="form-check-label">
+                              <?php echo "<input type=\"checkbox\" name=\"auto\" class=\"form-check-input\" value=\"1\"" . ($hab_pend["auto"] ? "checked" : "") . ">"; ?>
+                              Automática
+                            </label>
+                          </div>
+                          <div class="col-sm">
+                            <label class="form-check-label">
+                              <?php echo "<input type=\"checkbox\" name=\"gratis\" class=\"form-check-input\" value=\"1\"" . ($hab_pend["gratis"] ? "checked" : "") . ">"; ?>
+                              Gratuíta
+                            </label>
+                          </div>
+                          <div class="col-sm">
+                            <label class="form-check-label">
+                              <input type="checkbox" name="revisar" id="iRevisar" class="form-check-input" value="1">
+                              Pendiente de revisión
+                            </label>
+                          </div>
+                        </div>
+                      </div>
+                      <br>
+                      <div class="row">
+                        <div class="col-sm">
+                          <label for="iDescr">Efecto</label>
+                          <?php echo "<textarea class=\"form-control\" name=\"descr\" rows=\"6\">" . utf8_encode($hab_pend["descr"]) . "</textarea>" ?>
+                        </div>
+                      </div>
+                      <br>
+                      <div class="row">
+                        <div class="col-sm">
+                          <input type="submit" class="btn btn-primary" value="Guardar">
+                        </div>
+                      </div>
+                    </div>
+
+                    <hr>
+                  <?php }
+                }
+              ?>
+
+            </div>
+
+            <!-- Añadir hechizos -->
+            <div id="nueva" class="container-fluid tab-pane form-group">
               <div class="row">
                 <div class="col-sm-8">
                   <label for="iNom">Nombre:</label>
@@ -114,7 +226,7 @@
                 <div class="form-check">
                   <div class="col-sm">
                     <label class="form-check-label">
-                      <input type="checkbox" name="cont" id="iCont" class="form-check-input" value="1">
+                      <input type="checkbox" name="contin" id="iCont" class="form-check-input" value="1">
                       Continua
                     </label>
                   </div>
@@ -130,19 +242,25 @@
                       Gratuíta
                     </label>
                   </div>
+                  <div class="col-sm">
+                    <label class="form-check-label">
+                      <input type="checkbox" name="revisar" id="iRevisar" class="form-check-input" value="1" checked>
+                      Pendiente de revisión
+                    </label>
+                  </div>
                 </div>
               </div>
               <br>
               <div class="row">
                 <div class="col-sm">
                   <label for="iDescr">Efecto</label>
-                  <textarea class="form-control" id="iDescr" name="descr"></textarea>
+                  <textarea class="form-control" id="iDescr" name="descr" rows="6"></textarea>
                 </div>
               </div>
               <br>
               <div class="row">
                 <div class="col-sm">
-                  <input type="submit" class="btn btn-primary">
+                  <input type="submit" class="btn btn-primary" value="Guardar">
                 </div>
               </div>
             </div>
