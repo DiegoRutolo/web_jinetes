a4db732cae741ce4121d291c298c8059387646d4
DiegoRutolo <diegorutolo@gmail.com>
2019-11-30 13:23:00 +0100

    Alt al boton de inicio

--- res/herramientas.php
+++ res/herramientas.php
@@ -11,7 +11,7 @@
         <ul class="nav navbar-nav">
           <li class="nav-item">
             <a class="nav-link" href="https://jinetes.rutolo.eu">
-              <img src="https://jinetes.rutolo.eu/res/img/torre_blanca.png" class="icono" width="30em" height="30em">
+              <img src="https://jinetes.rutolo.eu/res/img/torre_blanca.png" class="icono" width="30em" height="30em" alt="Inicio">
             </a>
           </li>
           <li class="nav-item dropdown">
