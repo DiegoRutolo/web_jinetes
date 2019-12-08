75d4b31ea42bd52fee8f6b4b94d6dc2291d7770b
DiegoRutolo <diegorutolo@gmail.com>
2019-12-08 18:05:08 +0100

    Añadidos ingredientes de pociones

--- sistema/items.php
+++ sistema/items.php
@@ -33,6 +33,7 @@
   <body>
 
     <?php ImprimeNavBar(); ?>
+    <span id="arriba"></span>
 
     <div class="container">
       <?php
@@ -82,7 +83,42 @@
            <?php while ($ingrediente = $res_ingredientes->fetch_assoc()) { ?>
              <dl>
                <dt>
-                 <a href="#ingrediente-<?=$ingrediente["id"];?>"><?=utf8_encode($ingrediente["nom"]);?></a>
+                 <a data-toggle="modal" data-target="#ingrediente-<?=$ingrediente["id"];?>"><?=utf8_encode($ingrediente["nom"]);?></a>
+
+                 <!-- Modal -->
+                 <div id="ingrediente-<?=$ingrediente["id"];?>" class="modal fade">
+                   <div class="modal-dialog">
+                     <div class="modal-content">
+                       <div class="modal-header">
+                         <h4 class="modal-title"><?=utf8_encode($ingrediente["nom"]);?></h4>
+                         <button type="button" class="close" data-dismiss="modal">×</button>
+                       </div>
+
+                       <div class="modal-body container-fluid" style="font-weight: normal">
+                         <div class="row">
+                           <?php if (strlen($ingrediente["descr"]) > 0) { ?>
+                           <div class="col">
+                             <h6>Descipción:</h6>
+                             <?=utf8_encode($ingrediente["descr"]);?>
+                           </div>
+                          <?php } ?>
+                          <?php if (strlen($ingrediente["local"]) > 0) { ?>
+                          <div class="col">
+                            <h6>Localización:</h6>
+                            <?=utf8_encode($ingrediente["local"]);?>
+                          </div>
+                         <?php } ?>
+                         <?php if (strlen($ingrediente["prop"]) > 0) { ?>
+                         <div class="col">
+                           <h6>Propiedades:</h6>
+                           <?=utf8_encode($ingrediente["prop"]);?>
+                         </div>
+                        <?php } ?>
+                        </div>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
                </dt>
                <dd>
                  (<?=utf8_encode($ingrediente["cant"]);?>) <?=utf8_encode($ingrediente["coment"]);?>
@@ -102,7 +138,33 @@
          }
          ?>
        </div>
+
+       <div id="ingredientes" class="container tab-pane">
+         <?php
+          $sql = "SELECT * FROM Ingredientes ORDER BY nom";
+          $res = $con->query($sql);
+
+          if ($res->num_rows > 0) {
+            while ($ing = $res->fetch_assoc()) { ?>
+              <div id="ingrediente-<?=$ing["id"];?>">
+                <?=utf8_encode($ing["nom"]);?>
+
+                Aspecto: <?=utf8_encode($ing["descr"]);?>
+                <br>
+                Localización: <?=utf8_encode($ing["local"]);?>
+                <br>
+                Propiedades: <?=utf8_encode($ing["prop"]);?>
+              </div>
+            <?php }
+          } ?>
+       </div>
      </div>
+    </div>
+
+    <div class="boton-subir">
+      <a href="#arriba">
+        <?= GetIcono("fu", 45) ?>
+      </a>
     </div>
   </body>
 </html>
