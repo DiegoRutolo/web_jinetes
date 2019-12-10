3ea2b3a07d84131e91e51f1c0248ee238f61d70f
DiegoRutolo <diegorutolo@gmail.com>
2019-12-10 10:01:23 +0100

    Eliminada URL de navbar

--- res/herramientas.php
+++ res/herramientas.php
@@ -10,44 +10,44 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="nav navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="https://jinetes.rutolo.eu">
-              <img src="https://jinetes.rutolo.eu/res/img/torre_blanca.png" class="icono" width="30em" height="30em" alt="Inicio">
+            <a class="nav-link" href="/">
+              <img src="/res/img/torre_blanca.png" class="icono" width="30em" height="30em" alt="Inicio">
             </a>
           </li>
           <li class="nav-item dropdown">
             <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#">Sistema y contenido <span class="caret"></span></a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="https://jinetes.rutolo.eu/sistema/LosJinetesDeKal_manual.pdf" target="_blank">Manual (pdf)</a>
-              <a class="dropdown-item" href="https://jinetes.rutolo.eu/sistema/hechizos.php">Hechizos y Habilidades</a>
-              <a class="dropdown-item" href="https://jinetes.rutolo.eu/sistema/items.php">Objetos</a>
+              <a class="dropdown-item" href="/sistema/LosJinetesDeKal_manual.pdf" target="_blank">Manual (pdf)</a>
+              <a class="dropdown-item" href="/sistema/hechizos.php">Hechizos y Habilidades</a>
+              <a class="dropdown-item" href="/sistema/items.php">Objetos</a>
             </div>
           </li>
           <li class="nav-item dropdown">
             <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#">Herramientas <span class="caret"></span></a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="https://jinetes.rutolo.eu/herramientas/genHojasHabs">Generador de hojas</a>
+              <a class="dropdown-item" href="/herramientas/genHojasHabs">Generador de hojas</a>
             </div>
           </li>
           <li class="nav-item dropdown">
             <a class="dropdown-toggle nav-link" data-toggle="dropdown" href="#">Multimedia <span class="caret"></span></a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="https://jinetes.rutolo.eu/multimedia/dibujos.php">Dibujos</a>
-              <a class="dropdown-item" href="https://jinetes.rutolo.eu/multimedia/musica.php">Música</a>
+              <a class="dropdown-item" href="/multimedia/dibujos.php">Dibujos</a>
+              <a class="dropdown-item" href="/multimedia/musica.php">Música</a>
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="https://jinetes.rutolo.eu/gente">Personas</a>
+            <a class="nav-link" href="/gente">Personas</a>
           </li>
         </ul>
 
         <ul class="nav navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link" href="https://jinetes.rutolo.eu/fichaWeb/ficha.html" target="_blank">Ficha web</a>
+          <a class="nav-link" href="/fichaWeb/ficha.html" target="_blank">Ficha web</a>
         </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Administración <span class="caret"></span></a>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="https://jinetes.rutolo.eu/admin/index.php">Panel</a>
+              <a class="dropdown-item" href="/admin/index.php">Panel</a>
               <a class="dropdown-item" href="https://trello.com/b/UEebTZMS/jinetes" target="_blank">Trello</a>
             </div>
           </li>
@@ -61,15 +61,15 @@
   {
     $url = "";
     if ($val === "t") {
-      $url = "https://jinetes.rutolo.eu/res/img/green_tick.png";
+      $url = "/res/img/green_tick.png";
     } elseif ($val === "c") {
-      $url = "https://jinetes.rutolo.eu/res/img/red_cross.png";
+      $url = "/res/img/red_cross.png";
     } elseif ($val === "e") {
-      $url = "https://jinetes.rutolo.eu/res/img/edit.jpg";
+      $url = "/res/img/edit.jpg";
     } elseif ($val == "fu") {
-      $url = "https://jinetes.rutolo.eu/res/img/flechaArriba.png";
+      $url = "/res/img/flechaArriba.png";
     } elseif ($val == "www") {
-      $url = "https://jinetes.rutolo.eu/res/img/www.png";
+      $url = "/res/img/www.png";
     }
 
     $imagen = "<img src=\"" . $url . "\" class=\"icono\" width=\"" . $tam . "em\" height=\"" . $tam . "em\">";
